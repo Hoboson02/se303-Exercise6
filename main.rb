c379c22aceec9df01edc78abf2f832cb6e5856f9
Ryan Earl <ryanleoearl@gmail.com>
2022-05-08 23:45:46 -0700

    Removal of unecessary parameters from call to paint method in draw_button

--- main.rb
+++ main.rb
@@ -8,14 +8,14 @@ class Draw
   end
 
   def paint(color, color_value)
-    
+
   end
 
   def draw_button(foreground_color, is_dark_mode)
     if is_dark_mode
-      paint(label_text, x, y, foreground_color - 10, '#111111')
+      paint(foreground_color - 10, '#111111')
     else
-      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+      paint(foreground_color + 10, '#E0E0E0')
     end
   end
 end
