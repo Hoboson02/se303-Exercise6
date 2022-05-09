08b4f89e61695b31bdb64bc8bc21a460e3107fc3
Ryan Earl <ryanleoearl@gmail.com>
2022-05-08 23:34:17 -0700

    Combined functions into Draw class

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-  end
+class Draw
+  def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+    if is_dark_mode
+      paint(label_text, x, y, foreground_color - 10, '#111111')
+    else
+      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    end
+  endlllll
 end
